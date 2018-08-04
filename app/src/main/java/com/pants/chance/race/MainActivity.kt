0019a781f7827e400eac5d72fddb69e364d752f1
chance <chance.a.cyphers@gmail.com>
2018-08-04 18:04:01 -0400

    Hello world

---
+++ app/src/main/java/com/pants/chance/race/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pants.chance.race
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
