e654e8da4898219fd72dafd24eca27f562d793ae
chance <chance.a.cyphers@gmail.com>
2018-08-08 14:16:19 -0400

    Move distance travelled to another activity

---
+++ app/src/main/java/com/pants/chance/race/DistanceTravelledActivity.kt
@@ -0,0 +1,54 @@
+package com.pants.chance.race
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.distance_travelled.*
+
+class DistanceTravelledActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.distance_travelled)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        checkPermission()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                locationResult.locations.forEach { Trip.record(it) }
+                distanceText.text = """dist travelled: ${Trip.getDistance()}"""
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(createLocationRequest(), locationCallback, null)
+    }
+
+    private fun createLocationRequest(): LocationRequest {
+        return LocationRequest().apply {
+            interval = 1000
+            fastestInterval = 500
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private fun checkPermission() {
+        val hasntPermissions = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+
+        if (hasntPermissions) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 123
+            )
+        }
+    }
+}
--- app/src/main/java/com/pants/chance/race/MainActivity.kt
+++ app/src/main/java/com/pants/chance/race/MainActivity.kt
@@ -1,57 +1,52 @@
 package com.pants.chance.race
 
-import android.content.pm.PackageManager
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.location.*
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.AuthCallback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationCallback: LocationCallback
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        checkPermission()
-
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult ?: return
-                locationResult.locations.forEach { Trip.record(it) }
-                theText.text = """dist travelled: ${Trip.getDistance()}"""
-            }
+        loginButton.setOnClickListener { login() }
+        distanceTravelledButton.setOnClickListener {
+            startActivity(Intent(this, DistanceTravelledActivity::class.java))
         }
 
-        fusedLocationClient.requestLocationUpdates(createLocationRequest(), locationCallback, null)
     }
 
-    private fun createLocationRequest() : LocationRequest{
-        return LocationRequest().apply {
-            interval = 1000
-            fastestInterval = 500
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
+    private fun login() {
+//        WebAuthProvider.init(auth0)
+//            .withScheme("demo")
+//            .withAudience(
+//                String.format(
+//                    "https://%s/userinfo",
+//                    getString(R.string.com_auth0_domain)
+//                )
+//            )
+//            .start(this@MainActivity, object : AuthCallback {
+//                override fun onFailure(dialog: Dialog) {
+//                    // Show error Dialog to user
+//                }
+//
+//                override fun onFailure(exception: AuthenticationException) {
+//                    // Show error to user
+//                }
+//
+//                override fun onSuccess(credentials: Credentials) {
+//                    // Store credentials
+//                    // Navigate to your main activity
+//                }
+//            })
     }
-
-    private fun checkPermission() {
-        val hasntPermissions = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED
-
-        if (hasntPermissions) {
-            ActivityCompat.requestPermissions(
-                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 123
-            )
-        }
-    }
-
 
 }
