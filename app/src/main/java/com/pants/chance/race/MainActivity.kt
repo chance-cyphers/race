67201b461fac56f8520c6609f9b6e64d4449e734
chance <chance.a.cyphers@gmail.com>
2018-08-08 14:35:30 -0400

    Login

--- app/src/main/java/com/pants/chance/race/MainActivity.kt
+++ app/src/main/java/com/pants/chance/race/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.Auth0
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.provider.AuthCallback
 import com.auth0.android.provider.WebAuthProvider
@@ -13,9 +14,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth0: Auth0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth0 = Auth0(this)
+        auth0.isOIDCConformant = true
 
         loginButton.setOnClickListener { login() }
         distanceTravelledButton.setOnClickListener {
@@ -25,28 +31,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun login() {
-//        WebAuthProvider.init(auth0)
-//            .withScheme("demo")
-//            .withAudience(
-//                String.format(
-//                    "https://%s/userinfo",
-//                    getString(R.string.com_auth0_domain)
-//                )
-//            )
-//            .start(this@MainActivity, object : AuthCallback {
-//                override fun onFailure(dialog: Dialog) {
-//                    // Show error Dialog to user
-//                }
-//
-//                override fun onFailure(exception: AuthenticationException) {
-//                    // Show error to user
-//                }
-//
-//                override fun onSuccess(credentials: Credentials) {
-//                    // Store credentials
-//                    // Navigate to your main activity
-//                }
-//            })
+        WebAuthProvider.init(auth0)
+            .withScheme("demo")
+            .withAudience(
+                String.format(
+                    "https://%s/userinfo",
+                    getString(R.string.com_auth0_domain)
+                )
+            )
+            .start(this@MainActivity, object : AuthCallback {
+                override fun onFailure(dialog: Dialog) {
+                    runOnUiThread {
+                        helloText.text = """failure: $dialog"""
+                    }
+                }
+
+                override fun onFailure(exception: AuthenticationException) {
+                    runOnUiThread {
+                        helloText.text = """failure: ${exception.message}"""
+                    }
+                }
+
+                override fun onSuccess(credentials: Credentials) {
+                    runOnUiThread {
+                        helloText.text = """success!: ${credentials.accessToken}"""
+                    }
+                }
+            })
     }
 
 }
