adecc653d62a2610d3096c9947d1f4d7f381912e
chance <chance.a.cyphers@gmail.com>
2018-08-09 16:05:11 -0400

    Add hockeyapp support

--- app/src/main/java/com/pants/chance/race/MainActivity.kt
+++ app/src/main/java/com/pants/chance/race/MainActivity.kt
@@ -4,6 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import net.hockeyapp.android.UpdateManager
+import net.hockeyapp.android.CrashManager
+
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +22,43 @@ class MainActivity : AppCompatActivity() {
         distanceTravelledButton.setOnClickListener {
             startActivity(Intent(this, DistanceTravelledActivity::class.java))
         }
+        checkForUpdates()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        checkForCrashes()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        unregisterManagers()
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        unregisterManagers()
     }
 
     private fun logout() {
-        var intent = Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         intent.putExtra(LoginActivity.KEY_CLEAR_CREDENTIALS, true)
         startActivity(intent)
         finish()
     }
+
+    private fun checkForCrashes() {
+        CrashManager.register(this)
+    }
+
+    private fun checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this)
+    }
+
+    private fun unregisterManagers() {
+        UpdateManager.unregister()
+    }
+
 
 }
