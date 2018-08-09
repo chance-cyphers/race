a4ad49271f1ee0db68e56fdf13fe118478996e64
chance <chance.a.cyphers@gmail.com>
2018-08-09 10:15:03 -0400

    Add login screen

---
+++ app/src/main/java/com/pants/chance/race/LoginActivity.kt
@@ -0,0 +1,86 @@
+package com.pants.chance.race
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.authentication.storage.CredentialsManagerException
+import com.auth0.android.authentication.storage.SecureCredentialsManager
+import com.auth0.android.authentication.storage.SharedPreferencesStorage
+import com.auth0.android.callback.BaseCallback
+import com.auth0.android.provider.AuthCallback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import android.content.Intent
+import kotlinx.android.synthetic.main.login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        const val KEY_CLEAR_CREDENTIALS = "com.auth0.CLEAR_CREDENTIALS"
+        const val EXTRA_ACCESS_TOKEN = "com.auth0.ACCESS_TOKEN"
+        const val EXTRA_ID_TOKEN = "com.auth0.ID_TOKEN"
+    }
+
+    private val CODE_DEVICE_AUTHENTICATION = 22
+    private lateinit var auth0: Auth0
+    private lateinit var credentialsManager: SecureCredentialsManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth0 = Auth0(this)
+        auth0.isOIDCConformant = true
+        credentialsManager = SecureCredentialsManager(this, AuthenticationAPIClient(auth0), SharedPreferencesStorage(this))
+
+        if (intent.getBooleanExtra(KEY_CLEAR_CREDENTIALS, false)) {
+            credentialsManager.clearCredentials()
+        }
+
+        if (!credentialsManager.hasValidCredentials()) {
+            setContentView(R.layout.login)
+            loginButton.setOnClickListener { login() }
+        } else {
+            credentialsManager.getCredentials(object : BaseCallback<Credentials, CredentialsManagerException> {
+                override fun onFailure(error: CredentialsManagerException?) {
+                    finish()
+                }
+                override fun onSuccess(payload: Credentials?) {
+                    showNextActivity(payload)
+                }
+            })
+        }
+    }
+
+    private fun showNextActivity(credentials: Credentials?) {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        intent.putExtra(EXTRA_ACCESS_TOKEN, credentials?.accessToken)
+        intent.putExtra(EXTRA_ID_TOKEN, credentials?.idToken)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun login() {
+        WebAuthProvider.init(auth0)
+            .withScheme("demo")
+            .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
+            .withScope("openid offline_access")
+            .start(this@LoginActivity, object : AuthCallback {
+                override fun onFailure(dialog: Dialog) {
+                }
+
+                override fun onFailure(exception: AuthenticationException) {
+                }
+
+                override fun onSuccess(credentials: Credentials) {
+                    credentialsManager.saveCredentials(credentials)
+                    showNextActivity(credentials)
+                }
+            })
+    }
+
+
+}
--- app/src/main/java/com/pants/chance/race/MainActivity.kt
+++ app/src/main/java/com/pants/chance/race/MainActivity.kt
@@ -1,63 +1,28 @@
 package com.pants.chance.race
 
-import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.auth0.android.Auth0
-import com.auth0.android.authentication.AuthenticationException
-import com.auth0.android.provider.AuthCallback
-import com.auth0.android.provider.WebAuthProvider
-import com.auth0.android.result.Credentials
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth0: Auth0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth0 = Auth0(this)
-        auth0.isOIDCConformant = true
-
-        loginButton.setOnClickListener { login() }
+        logoutButton.setOnClickListener { logout() }
         distanceTravelledButton.setOnClickListener {
             startActivity(Intent(this, DistanceTravelledActivity::class.java))
         }
-
     }
 
-    private fun login() {
-        WebAuthProvider.init(auth0)
-            .withScheme("demo")
-            .withAudience(
-                String.format(
-                    "https://%s/userinfo",
-                    getString(R.string.com_auth0_domain)
-                )
-            )
-            .start(this@MainActivity, object : AuthCallback {
-                override fun onFailure(dialog: Dialog) {
-                    runOnUiThread {
-                        helloText.text = """failure: $dialog"""
-                    }
-                }
-
-                override fun onFailure(exception: AuthenticationException) {
-                    runOnUiThread {
-                        helloText.text = """failure: ${exception.message}"""
-                    }
-                }
-
-                override fun onSuccess(credentials: Credentials) {
-                    runOnUiThread {
-                        helloText.text = """success!: ${credentials.accessToken}"""
-                    }
-                }
-            })
+    private fun logout() {
+        var intent = Intent(this, LoginActivity::class.java)
+        intent.putExtra(LoginActivity.KEY_CLEAR_CREDENTIALS, true)
+        startActivity(intent)
+        finish()
     }
 
 }
